a4e230c3dd39c99d6dfd566749cf3631278ebddf
saranrajkannan27 <saran.kannan@fulcrum.net>
2016-12-19 00:29:57 +0530

    Saran -updated LoginActivity.java and RoutineObjectRepository.java object id's

--- CATS_Mobility/src/main/java/businessComponents/MOBILE/LoginActivity.java
+++ CATS_Mobility/src/main/java/businessComponents/MOBILE/LoginActivity.java
@@ -17,20 +17,20 @@ public class LoginActivity extends Utility implements RoutineObjectRepository {
 	}
 
 	public void addConnection() {
-		Click(By.name("Add Connection"), "Click - AddConnection");
-		Click(By.id("connections_add"), "Click - AddConnection Symbol");
-		EnterText(By.id("connection_edit_name"), "Enter - Connection Name", "AIRTEL QA2");
-		EnterText(By.id("connection_edit_host"), "Enter - Host", "services-uswest.skytap.com");
-		EnterText(By.id("connection_edit_port"), "Enter - Port", "25582");
-		Click(By.id("connection_edit_ssl"), "Click - Enable SSL");
-		Click(By.id("connections_save"), "Click - Save Connection");
-		Click(By.id("action_bar_title"), "Click - Back button");
+		Click(TAB_CONNECTIONS, "Click - AddConnection");
+		Click(ICON_ADD, "Click - AddConnection Symbol");
+		EnterText(TXT_NAME, "Enter - Connection Name", "AIRTEL QA2");
+		EnterText(TXT_HOST, "Enter - Host", "services-uswest.skytap.com");
+		EnterText(TXT_PORT, "Enter - Port", "25582");
+		Click(TOGGLE_BTN_SSL , "Click - Enable SSL");
+		Click(ICON_SAVE, "Click - Save Connection");
+		Click(IMG_BACK_BTN , "Click - Back button");
 	}
 
 	public void login() {
 		EnterText(TXT_USERNAME, "Enter - Username", "catsadm");
 		EnterText(TXT_PASSWORD, "Enter - Password", "catscats11");
 		HideKeyboard();
-		Click(By.id("btn_connect"), "Click - Connect button");
+		Click(BTN_CONNECT, "Click - Connect button");
 	}
 }
