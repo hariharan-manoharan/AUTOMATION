f39aec1f6c2299eb1de3ff93bc94445db901fc3e
hariharan-manoharan <hariharan.manoharan@fulcrum.net>
2016-12-18 16:29:35 +0530

    Removed sysout, Added test log

--- CATS_Mobility/src/main/java/utils/CopyLatestResult.java
+++ CATS_Mobility/src/main/java/utils/CopyLatestResult.java
@@ -7,48 +7,36 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 
+import com.relevantcodes.extentreports.LogStatus;
+
 import main.java.reporting.HtmlReport;
 
-public class CopyLatestResult {
+public class CopyLatestResult extends Utility {
 
 	public void copyLatestResultFolder() {
-
 		emptyLatestResultFolder(HtmlReport.relativePath + "/LatestResult");
-
 		File srcFolder = new File(HtmlReport.relativePath + "/Results/" + HtmlReport.reportFolderName);
 		File destFolder = new File(HtmlReport.relativePath + "/LatestResult");
 
-		// make sure source exists
 		if (!srcFolder.exists()) {
-
-			// System.out.println("Directory does not exist.");
-			// just exit
-			System.exit(0);
-
+			test.log(LogStatus.WARNING, "Folder " + HtmlReport.reportFolderName + " does not exists", "");
 		} else {
-
 			try {
 				copyFolder(srcFolder, destFolder);
 			} catch (IOException e) {
-				e.printStackTrace();
-				// error, just exit
-				System.exit(0);
+				test.log(LogStatus.WARNING, "Exception occured while copying result folder "
+						+ HtmlReport.reportFolderName + " to LatestResult folder", "");
+				test.log(LogStatus.WARNING, e);
 			}
 		}
-
-		// System.out.println("Done");
-
 	}
 
 	private void copyFolder(File src, File dest) throws IOException {
 
 		if (src.isDirectory()) {
 
-			// if directory not exists, create it
 			if (!dest.exists()) {
 				dest.mkdir();
-				// System.out.println("Directory copied from " + src + " to " +
-				// dest);
 			}
 
 			// list all the directory contents
@@ -61,7 +49,6 @@ public class CopyLatestResult {
 				// recursive copy
 				copyFolder(srcFile, destFile);
 			}
-
 		} else {
 			// if file, then copy it
 			// Use bytes stream to support all file types
@@ -78,71 +65,47 @@ public class CopyLatestResult {
 
 			in.close();
 			out.close();
-			// System.out.println("File copied from " + src + " to " + dest);
 		}
 	}
 
-	private void emptyLatestResultFolder(String SRC_FOLDER) {
-
-		File directory = new File(SRC_FOLDER);
-
+	private void emptyLatestResultFolder(String latestResult) {
+		File directory = new File(latestResult);
 		// make sure directory exists
 		if (!directory.exists()) {
-
-
+			// Do Nothing
 		} else {
-
 			try {
-
 				delete(directory);
-
 			} catch (IOException e) {
-				e.printStackTrace();
-				System.exit(0);
+				test.log(LogStatus.WARNING, "Exception occured while deleting LatestResult folder", "");
+				test.log(LogStatus.WARNING, e);
 			}
 		}
 
-		// System.out.println("Done");
 	}
 
 	public static void delete(File file) throws IOException {
-
 		if (file.isDirectory()) {
-
 			// directory is empty, then delete it
 			if (file.list().length == 0) {
-
 				file.delete();
-				// System.out.println("Directory is deleted : "
-				// + file.getAbsolutePath());
-
 			} else {
-
 				// list all the directory contents
 				String files[] = file.list();
-
 				for (String temp : files) {
 					// construct the file structure
 					File fileDelete = new File(file, temp);
-
 					// recursive delete
 					delete(fileDelete);
 				}
-
 				// check the directory again, if empty then delete it
 				if (file.list().length == 0) {
 					file.delete();
-					// System.out.println("Directory is deleted : "
-					// + file.getAbsolutePath());
 				}
 			}
-
 		} else {
 			// if file, then delete it
 			file.delete();
-			// System.out.println("File is deleted : " +
-			// file.getAbsolutePath());
 		}
 	}
-
 }
