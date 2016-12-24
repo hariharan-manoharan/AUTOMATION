aaf7635f52acc6ce52c3c8f1ee55b1e630300ddc
hariharan-manoharan <hariharan.manoharan@fulcrum.net>
2016-12-24 19:26:59 +0530

    Airtel Specific changes - To Fetch data from database based on Transfer Request Number

--- CATS_Mobility/src/main/java/businessComponents/MOBILE/AIRTEL/RoutinesActivity.java
+++ CATS_Mobility/src/main/java/businessComponents/MOBILE/AIRTEL/RoutinesActivity.java
@@ -1,8 +1,12 @@
 package main.java.businessComponents.MOBILE.AIRTEL;
 
+
+import java.util.LinkedHashMap;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.TimeoutException;
+import org.openqa.selenium.WebDriverException;
 
 import com.relevantcodes.extentreports.ExtentTest;
 
@@ -11,7 +15,13 @@ import main.java.testDataAccess.DataTable;
 import main.java.utils.Utility;
 
 public class RoutinesActivity extends Utility implements RoutineObjectRepository {
+	
 
+	String requestNumber = dataTable.getData("Transfer Request", "REQNUMBER");
+	String transferNumber  =  selectQuerySingleValue(String.format(TR_TRANSFER_NO_QUERY,requestNumber), "TRANSFERNUMBER");
+	String lineItemCount  =  selectQuerySingleValue(String.format(TR_LINEITEM_COUNT,requestNumber), "COUNT");
+	LinkedHashMap<String, String> lineItemDetails = selectQueryMultipleValues(String.format(TR_LINEITEM_DETAILS,requestNumber), "LINENUMBER#PARTID#ASSETID");
+			
 	@SuppressWarnings("rawtypes")
 	public RoutinesActivity(ExtentTest test, AndroidDriver driver, DataTable dataTable) {
 		super(test, driver, dataTable);
@@ -33,44 +43,105 @@ public class RoutinesActivity extends Utility implements RoutineObjectRepository
 
 	}
 
-	public void pick() throws TimeoutException, NoSuchElementException {
+	public void pick() throws TimeoutException, NoSuchElementException, WebDriverException {
+
+		String fromLocation = selectQuerySingleValue(String.format(TR_FROM_LOC_QUERY, requestNumber), "NAME");
+		String lineItem = null;
+		boolean serializedItem = false;
+
 		selectRoutine("Pick");
 		String text = GetText(ID_ACTION_BAR_SUBTITLE, "Routine name");
+		
 		if (text.equals("Pick")) {
-			EnterTextWebView(XPATH_TXT_WEBVIEW, "Enter Location Name", "BAL-MUNDKA-MDEL");
+			
+			EnterText(XPATH_TXT, "Enter Location Name", fromLocation);
 			ClickNext();
-			EnterTextWebView(XPATH_TXT_WEBVIEW, "Enter Transfer Request (*)", "T000000003");
+			EnterText(XPATH_TXT, "Enter Transfer Request (*)", transferNumber);
 			ClickNext();
-			EnterTextWebView(XPATH_TXT_WEBVIEW, "Enter Line/Item # (*)", "TESTASSET000001");
-			ClickNext();
-			EnterTextWebView(XPATH_TXT_WEBVIEW, "Enter Notes", "Pick - Test Notes");
-			ClickNext();			
+
+			for (int line = 1; line <= Integer.parseInt(lineItemCount); line++) {
+
+				if (lineItemDetails.get("ASSETID_" + line) != null) {
+					lineItem = selectQuerySingleValue(String.format(TR_ASSETCODE_QUERY, requestNumber), "ASSETCODE");
+					serializedItem = true;
+				} else {
+					lineItem = selectQuerySingleValue(String.format(TR_PARTCODE_QUERY, String.valueOf(line), requestNumber), "PARTCODE");
+					serializedItem = false;
+				}
+				
+				EnterText(XPATH_TXT, "Enter Line/Item # (*) - "+line, lineItem);
+				ClickNext();
+
+				if (serializedItem) {					
+					EnterText(XPATH_TXT, "Enter Notes", "Pick - Test Notes");
+					ClickNext();
+				} else {
+					ClickNext();
+					ClickNext();
+					ClickNext();
+					EnterText(XPATH_TXT, "Enter Notes", "Pick - Test Notes");
+					ClickNext();
+				}
+
+			}
+
 			if (isObjectPresent(ID_MESSAGE, "Transcation completed message")) {
 				GetText(ID_MESSAGE, "Transcation completed message");
 				Click(ID_MESSAGE_OK, "Click OK");
 			}
 			Click(CONTENT_DESC_ROUITNE_BACK_BTN, "Click Rouitne Back Button");
+
 		}
 
 	}
 
-	public void pack() throws TimeoutException, NoSuchElementException  {
+	public void pack() throws TimeoutException, NoSuchElementException, WebDriverException {
+
+		String fromLocation = selectQuerySingleValue(String.format(TR_FROM_LOC_QUERY, requestNumber), "NAME");
+		String lineItem = null;
+		boolean serializedItem = false;
+
 		selectRoutine("Pack");
 		String text = GetText(ID_ACTION_BAR_SUBTITLE, "Routine name");
 		if (text.equals("Pack")) {
-			EnterTextWebView(XPATH_TXT_WEBVIEW, "Enter Location Name", "BAL-MUNDKA-MDEL");
+			
+			EnterText(XPATH_TXT, "Enter Location Name", fromLocation);
 			ClickNext();
-			EnterTextWebView(XPATH_TXT_WEBVIEW, "Enter Transfer Request (*)", "T000000003");
+			EnterText(XPATH_TXT, "Enter Transfer Request (*)", transferNumber);
 			ClickNext();
 			if (GetText(ID_MESSAGE, "Confirm Message").equalsIgnoreCase("Generate new shipment?")) {
 				Click(ID_MESSAGE_CONFIRM_YES, "Clicked 'Yes' for 'Generate new shipment?' message");
-				// GetTextWebView(XPATH_TXT_WEBVIEW, "Shipment#");
+
 			}
 			ClickNext();
-			EnterTextWebView(XPATH_TXT_WEBVIEW, "Enter Barcode (*)", "TESTASSET000001");
-			ClickNext();
-			EnterTextWebView(XPATH_TXT_WEBVIEW, "Enter Notes", "Pack - Test Notes");
-			ClickNext();			
+
+			for (int line = 1; line <= Integer.parseInt(lineItemCount); line++) {
+
+				if (lineItemDetails.get("ASSETID_" + line) != null) {
+					lineItem = selectQuerySingleValue(String.format(TR_ASSETCODE_QUERY, requestNumber), "ASSETCODE");
+					serializedItem = true;
+				} else {
+					lineItem = selectQuerySingleValue(
+							String.format(TR_PARTCODE_QUERY, String.valueOf(line), requestNumber), "PARTCODE");
+					serializedItem = false;
+				}
+
+				EnterText(XPATH_TXT, "Enter Line/Item # (*) - "+line, lineItem);
+				ClickNext();
+
+				if (serializedItem) {
+					EnterText(XPATH_TXT, "Enter Notes", "Pack - Test Notes");
+					ClickNext();
+				} else {
+					ClickNext();
+					ClickNext();
+					ClickNext();
+					EnterText(XPATH_TXT, "Enter Notes", "Pack - Test Notes");
+					ClickNext();
+				}
+
+			}
+
 			if (isObjectPresent(ID_MESSAGE, "Transcation completed message")) {
 				GetText(ID_MESSAGE, "Transcation completed message");
 				Click(ID_MESSAGE_OK, "Click OK");
@@ -79,26 +150,32 @@ public class RoutinesActivity extends Utility implements RoutineObjectRepository
 		}
 	}
 
-	public void ship() throws TimeoutException, NoSuchElementException {
+	public void ship() throws TimeoutException, NoSuchElementException,  WebDriverException {
+		
+		String fromLocation = selectQuerySingleValue(String.format(TR_FROM_LOC_QUERY,requestNumber),"NAME");
+		String ShipmentNumber = selectQuerySingleValue(String.format(TR_SHIPMENT_NO_QUERY,requestNumber),"SHIPMENTNUMBER");
+		int shipmentCount  =  Integer.parseInt(selectQuerySingleValue(String.format(TR_SHIPMENT_COUNT,requestNumber), "COUNT"));
+		
 		selectRoutine("Ship");
 		String text = GetText(ID_ACTION_BAR_SUBTITLE, "Routine name");
 		if (text.equals("Ship")) {
-			EnterTextWebView(XPATH_TXT_WEBVIEW, "Enter Location Name", "BAL-MUNDKA-MDEL");
+			EnterText(XPATH_TXT, "Enter Location Name", fromLocation);
 			ClickNext();
-			EnterTextWebView(XPATH_TXT_WEBVIEW, "Enter Transfer Request (*)", "T000000003");
+			EnterText(XPATH_TXT, "Enter Transfer Request (*)", transferNumber);
 			ClickNext();
-			EnterTextWebView(XPATH_TXT_WEBVIEW, "Enter Shipment Method (*)", "AIRWAYS");
+			EnterText(XPATH_TXT, "Enter Shipment Method (*)", "AIRWAYS");
 			ClickNext();
-			EnterTextWebView(XPATH_TXT_WEBVIEW, "Enter Tracking Number", "TEST");
-			ClickNext();
-			EnterTextWebView(XPATH_TXT_WEBVIEW, "Enter Shipment Number (*)", "000000000020161220-6");
-			ClickNext();
-			EnterTextWebView(XPATH_TXT_WEBVIEW, "Enter Notes", "Ship - Test Notes");
-			ClickNext();
-			if (isObjectPresent(ID_MESSAGE, "Transcation completed message")) {
-				GetText(ID_MESSAGE, "Transcation completed message");
-				Click(ID_MESSAGE_OK, "Click OK");
+			EnterText(XPATH_TXT, "Enter Tracking Number", "TEST");
+			ClickNext();	
+			
+			if(shipmentCount!=1){
+				EnterText(XPATH_TXT, "Enter Shipment Number (*)", ShipmentNumber);			
+				ClickNext();
 			}
+			
+			ClickNext();
+			EnterText(XPATH_TXT, "Enter Notes", "Ship - Test Notes");
+			ClickNext();
 			Click(CONTENT_DESC_ROUITNE_BACK_BTN, "Click Rouitne Back Button");
 		}
 	}
